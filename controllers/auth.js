9bd18b9e1ceb6ff4e4e301d4dd7a420ed75adfd9
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-09 21:30:41 +0100

    feature: Sign in new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -11,5 +12,22 @@ exports.signup = (req, res, next) => {
     throw error;
   }
   const { email, name, password } = req.body;
-  
+  bcrypt.hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        email,
+        name,
+        password: hashedPw,
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({ message: 'User created!', userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
