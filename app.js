212ca62e344203b2cd0670662e310b3f524b4667
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-11 00:16:40 +0100

    feature: Initialize socket.io

--- app.js
+++ app.js
@@ -52,6 +52,15 @@ app.use((error, req, res, next) => {
 mongoose.connect(MONGODB_URI)
   .then(result => {
     console.log('Connected!')
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server, {
+      cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+      }
+    });
+    io.on('connection', socket => {
+      console.log('Client connected')
+    });
   })
   .catch(err => console.log(err));
